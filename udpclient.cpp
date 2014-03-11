1fd072b7c506bae487a756b9e316e6aea4e1f343
kg-it-github <zhang7271@qq.com>
2014-03-11 22:11:12 +0800

    udpserver and udpclient

--- udpclient.cpp
+++ udpclient.cpp
@@ -1,1 +1,11 @@
+#include "mysock.h"
 
+int f_udpclient()
+{
+    CSockUdpServer server("127.0.0.1", 8000);
+    CSockUdpClient client;
+    client.SockCreate();
+    char buf[1024] = "hello server";
+    client.SendTo(client.GetSock(),buf , strlen(buf), 0, &server);
+    return 0;
+}
--- udpserver.cpp
+++ udpserver.cpp
@@ -1,1 +1,28 @@
+#include "mysock.h"
 
+int f_udpserver()
+{
+    CSockUdpServer udpserver;
+    CSockUdpClient udpclient;
+    udpserver.Listen();
+
+    while(true)
+    {
+        char buf[1024] = {0};
+        int len;
+        len = udpserver.RecvFrom(udpserver.GetSock(), buf, sizeof(buf), 0, &udpclient);
+        if (len > 0)
+        {
+            cout << "recvfrom addr : " << udpclient.GetAddr() << endl;
+            cout << "recvfrom port : " << udpclient.GetPort() << endl;
+            cout << "recvfrom len : " << len << endl;
+            cout << "recvfrom data : " << buf << endl;
+            cout << endl;
+        }
+        usleep(200);
+
+    }
+
+
+    return 0;
+}
